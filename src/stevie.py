51c2cb2bf778fad596cd391ed62be0bd62552bed
Marten de Bruijn <martendebruijn@outlook.com>
2024-10-28 23:03:36 +0100

    docs: add doc comments to stevie

--- src/stevie.py
+++ src/stevie.py
@@ -3,26 +3,36 @@ import requests
 import argparse
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
 
+# Get environment variables
 api_key = os.getenv("API_KEY")
 device_id = os.getenv("DEVICE_ID")
 
+# Set base URL and headers
 base_url = f"https://api.smartthings.com/v1/devices/{device_id}/commands"
 headers = {"Authorization": f"Bearer {api_key}", "Content-Type": "application/json"}
 
+# Parse command line arguments§
 parser = argparse.ArgumentParser(description="Control Stevie the TV")
 
+# Add arguments
 parser.add_argument(
     "--turn", type=str, help="Turn Stevie on or off", choices=["on", "off"]
 )
 
 parser.add_argument("--volume", type=int, help="Set Stevie's volume (0-100)")
+
+# Parse arguments
 args = parser.parse_args()
+
+# Functions to control Stevie:
 
 
 # Capability: Switch
 # See: https://developer.smartthings.com/docs/devices/capabilities/capabilities-reference#switch
+# Command: on, off
 def turn_stevie(command):
     data = {
         "commands": [{"component": "main", "capability": "switch", "command": command}]
